3b00e12605d1fde96858cb5cf4b78ecfe50802ea
vinci <vincijin0212@gmail.com>
2016-06-04 16:31:52 -0500

    add footer diretory and modify main css

---
+++ app/directives/footerDirective.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vinci on 6/4/16.
+ */
+/**
+ * Created by vinci on 6/4/16.
+ */
+angular.module("myApp").directive("footerDirective", function() {
+    return {
+        templateUrl : "views/share/footer.html"
+    };
+});
