fcc8e8889df492781d5e1c047ca1f7c236a4a892
Xu Liang <obdark1988@gmail.com>
2016-06-07 17:23:03 -0500

    create signup controller logic

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,7 +1,36 @@
 /**
  * Created by obdark on 6/3/16.
  */
-angular.module('myApp').controller('loginController', function($scope) {
-    $scope.username= "A12345";
-    $scope.password= "012345";
+angular.module('myApp').controller('loginController', function($scope, $http) {
+    $scope.username = 'xiaoyu';
+    $scope.password = 'qxy123';
+    $scope.password_confirmation = 'qxy123';
+    $scope.request = {
+        method: 'post',
+        url: 'http://more-gold.com/user-api/public/index.php/api/account/create-user',
+        data: {
+            'grant_type': 'password',
+            'client_id': 'dqLWBDS924bAKf0elB',
+            'client_secret': 'aefxrpHksbgyzm4dyxoVcRyjJDcEoKjKchPP',
+            'username': $scope.username,
+            'password': $scope.password,
+            'password_confirmation': $scope.password_confirmation,
+            'role_id': 1
+        },
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    };
+
+    $scope.signup = function()
+    {
+        $http($scope.request).then(
+            function onSuccess(response){
+                console.log(response.data);
+            },
+            function onError(response){
+                console.log(response.data);
+            }
+        );
+    }
 });
--- app/controllers/signUpController.js
+++ app/controllers/signUpController.js
@@ -1,9 +1,37 @@
 /**
  * Created by Xiaoyu on 6/4/2016.
  */
-angular.module('myApp').controller('sighUpController', function($scope) {
-    $scope.username= "A12345";
-    $scope.password= "012345";
-    $scope.conffPassword= "012345";
-    $scope.emailAdd= "A12345@gmail.com";
+angular.module('myApp').controller('signUpController', function($scope, $http, $location) {
+    $scope.username = 'xiaoyu11@163.com';
+                $scope.password = 'qxy123';
+                $scope.password_confirmation = 'qxy123';
+
+                $scope.request = {
+                    method: 'post',
+                    url: 'http://more-gold.com/user-api/public/index.php/api/account/create-user',
+                    data: {
+                        'grant_type': 'password',
+                        'client_id': 'dqLWBDS924bAKf0elB',
+                        'client_secret': 'aefxrpHksbgyzm4dyxoVcRyjJDcEoKjKchPP',
+                        'username': $scope.username,
+                        'password': $scope.password,
+                        'password_confirmation': $scope.password_confirmation,
+                        'role_id': 1
+                    },
+                    headers: {
+                        'Content-Type': 'application/json; charset=utf-8'
+                    }
+                };
+
+                $scope.signup = function()
+                {
+                    $http($scope.request).then(
+                        function onSuccess(response){
+                            $location.path('/userInfo');
+                        },
+                        function onError(response){
+                            $scope.errorMessage = response.data.message;
+            }
+        );
+    }
 });
